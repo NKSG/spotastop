025271f41ae82f64cebad18b57e98a0ffa27d5a8
Nicola <n.sacco.dev@gmail.com>
2013-10-26 00:08:16 +0200

    UI

--- src/com/nicfix/stopspot/SelectBusLine.java
+++ src/com/nicfix/stopspot/SelectBusLine.java
@@ -5,10 +5,10 @@ import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.util.AttributeSet;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import com.nicfix.stopspot.domain.Line;
 import com.nicfix.stopspot.presentation.BusLineItem;
@@ -19,20 +19,18 @@ public class SelectBusLine extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_select_bus_line);
-		inflater = (LayoutInflater) getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 		Line l=new Line();
-		l.setName("11Comma bis");
-		
-		BusLineItem lineItem= new BusLineItem(this);
-		lineItem.setLine(l);
+		l.setName("11");
+		BusLineItem lineItem = new BusLineItem(SelectBusLine.this);
+		lineItem.setLayoutParams(new LinearLayout.LayoutParams(
+				LinearLayout.LayoutParams.WRAP_CONTENT,
+				LinearLayout.LayoutParams.WRAP_CONTENT));
 		((ViewGroup) findViewById(R.id.busList)).addView(lineItem);
-		
-
+		lineItem.setLine(l);
 	}
 
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.select_bus_line, menu);
 		return true;
 	}
@@ -42,9 +40,11 @@ public class SelectBusLine extends Activity {
 		@Override
 		public void onReceive(Context context, Intent intent) {
 			for (Line l : StopSpotApp.getInstance().getLinesList()) {
-				BusLineItem lineItem = (BusLineItem) inflater.inflate(
-						R.layout.bus_line_item,
-						(ViewGroup) findViewById(R.id.busList), true);
+				BusLineItem lineItem = new BusLineItem(SelectBusLine.this);
+				lineItem.setLayoutParams(new LinearLayout.LayoutParams(
+						LinearLayout.LayoutParams.WRAP_CONTENT,
+						LinearLayout.LayoutParams.WRAP_CONTENT));
+				((ViewGroup) findViewById(R.id.busList)).addView(lineItem);
 				lineItem.setLine(l);
 			}
 		}
--- src/com/nicfix/stopspot/presentation/BusLineItem.java
+++ src/com/nicfix/stopspot/presentation/BusLineItem.java
@@ -1,33 +1,56 @@
 package com.nicfix.stopspot.presentation;
 
 import android.content.Context;
+import android.content.Intent;
 import android.util.AttributeSet;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.nicfix.stopspot.LoginActivity;
 import com.nicfix.stopspot.R;
+import com.nicfix.stopspot.SelectBusLine;
+import com.nicfix.stopspot.SpotActivity;
+import com.nicfix.stopspot.StopSpotApp;
 import com.nicfix.stopspot.domain.Line;
 
-public class BusLineItem extends View {
+public class BusLineItem extends LinearLayout {
 
 	private TextView lineName;
 
 	private TextView lineDirection;
 
+	private LayoutInflater inflater;
+
+	private Line line;
+
 	public BusLineItem(Context context) {
 		super(context);
-		
+		inflater = (LayoutInflater) context
+				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+		inflater.inflate(R.layout.bus_line_item, this, true);
 	}
 
 	public void setLine(Line l) {
-		/*
+
+		this.line = l;
 		this.lineName = (TextView) findViewById(R.id.lineName);
 		this.lineDirection = (TextView) findViewById(R.id.lineDirection);
+
 		this.lineName.setText(l.getName());
 		if (l.getPath().size() > 0)
 			this.lineDirection.setText(l.getPath().get(l.getPath().size() - 1)
 					.getBusStop().getCode());
-				*/
+
+		this.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent i = new Intent(getContext(), SpotActivity.class);
+				getContext().startActivity(i);
+			}
+		});
 	}
 
 }
